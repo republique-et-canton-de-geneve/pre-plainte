65920fdbc35e3232d6a218c2d4d700100fd1a334
tissote <elise.tissot@etat.ge.ch>
2026-05-29 09:57:24 +0200

    PPEL-421 : Validation des numéros de plaque d'immatriculation
    # Conflicts:
    #	pre-plainte-ihm/src/schemas/incident-evenement.schema.ts
    
    # Conflicts:
    #	pre-plainte-core/src/test/java/ch/ge/police/core/domain/model/event/vol/common/ObjetIncidentTest.java
    #	pre-plainte-ihm/src/schemas/incident-evenement.schema.ts

--- pre-plainte-core/src/test/java/ch/ge/police/core/domain/model/event/vol/common/ObjetIncidentTest.java
+++ pre-plainte-core/src/test/java/ch/ge/police/core/domain/model/event/vol/common/ObjetIncidentTest.java
@@ -10,14 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ObjetIncidentTest {
 
+  private static final RipolCode SUISSE = new RipolCode("8100", "Suisse");
+  private static final RipolCode FRANCE = new RipolCode("8212", "France");
+
   @Test
   void shouldTreatPlaqueAsNonVehicleEvenWhenVehicleFlagIsTrue() {
     ObjetIncident objet = ObjetIncident.builder()
-        .categorieObjet("plaque")
-        .isVehicle(true)
-        .plaquePays(new RipolCode("8100", "Suisse"))
-        .plaqueNumero("GE123456")
-        .build();
+      .categorieObjet("plaque")
+      .isVehicle(true)
+      .plaquePays(SUISSE)
+      .plaqueNumero("GE123456")
+      .build();
 
     assertFalse(objet.isVehicleType());
   }
@@ -34,22 +37,88 @@ class ObjetIncidentTest {
   }
 
   @Test
-  void shouldAcceptPlaqueWithPlateNumberWithoutObjectTypeOrDescription() {
+  void shouldAcceptSwissPlate() {
     ObjetIncident objet = ObjetIncident.builder()
-        .categorieObjet("plaque")
-        .plaquePays(new RipolCode("8100", "Suisse"))
-        .plaqueNumero("GE123456")
-        .build();
+      .categorieObjet("plaque")
+      .plaquePays(SUISSE)
+      .plaqueNumero("GE123456")
+      .build();
 
     assertDoesNotThrow(objet::champsObligatoire);
   }
 
   @Test
+  void shouldRejectInvalidSwissPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(SUISSE)
+      .plaqueNumero("123GE")
+      .build();
+
+    assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldAcceptFrenchSivPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(FRANCE)
+      .plaqueNumero("AA-123-AA")
+      .build();
+
+    assertDoesNotThrow(objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldAcceptFrenchFniPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(FRANCE)
+      .plaqueNumero("123 ABC 75")
+      .build();
+
+    assertDoesNotThrow(objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldRejectInvalidFrenchPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(FRANCE)
+      .plaqueNumero("INVALID")
+      .build();
+
+    assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldAcceptInternationalPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(new RipolCode("9999", "Autre"))
+      .plaqueNumero("ABC123")
+      .build();
+
+    assertDoesNotThrow(objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldRejectInvalidInternationalPlate() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .categorieObjet("plaque")
+      .plaquePays(new RipolCode("9999", "Autre"))
+      .plaqueNumero("ABC-123")
+      .build();
+
+    assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
   void shouldRejectPlaqueWithoutPlateNumber() {
     ObjetIncident objet = ObjetIncident.builder()
-        .categorieObjet("plaque")
-        .plaquePays(new RipolCode("8100", "Suisse"))
-        .build();
+      .categorieObjet("plaque")
+      .plaquePays(SUISSE)
+      .build();
 
     assertThrows(ValidationMetierException.class, objet::champsObligatoire);
   }
@@ -57,9 +126,17 @@ class ObjetIncidentTest {
   @Test
   void shouldRejectPlaqueWithoutPlateCountry() {
     ObjetIncident objet = ObjetIncident.builder()
-        .categorieObjet("plaque")
-        .plaqueNumero("GE123456")
-        .build();
+      .categorieObjet("plaque")
+      .plaqueNumero("GE123456")
+      .build();
+
+    assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldRejectObjectWithoutType() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .build();
 
     assertThrows(ValidationMetierException.class, objet::champsObligatoire);
   }
@@ -67,9 +144,29 @@ class ObjetIncidentTest {
   @Test
   void shouldAcceptObjectWithoutDescription() {
     ObjetIncident objet = ObjetIncident.builder()
-        .type(new RipolCode("713103", "Telephone mobile"))
-        .numeroIMEIInconnu(true)
-        .build();
+      .type(new RipolCode("713103", "Telephone mobile"))
+      .numeroIMEIInconnu(true)
+      .build();
+
+    assertDoesNotThrow(objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldRejectMobilePhoneWithoutImei() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .type(new RipolCode("713103", "Telephone mobile"))
+      .numeroIMEIInconnu(false)
+      .build();
+
+    assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldAcceptValidImei() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .type(new RipolCode("713103", "Telephone mobile"))
+      .numeroIMEI("123456789012345")
+      .build();
 
     assertDoesNotThrow(objet::champsObligatoire);
   }
@@ -77,9 +174,9 @@ class ObjetIncidentTest {
   @Test
   void shouldRejectInvalidImeiFormat() {
     ObjetIncident objet = ObjetIncident.builder()
-        .type(new RipolCode("713103", "Telephone mobile"))
-        .numeroIMEI("IMEI123")
-        .build();
+      .type(new RipolCode("713103", "Telephone mobile"))
+      .numeroIMEI("IMEI123")
+      .build();
 
     assertThrows(ValidationMetierException.class, objet::champsObligatoire);
   }
@@ -87,9 +184,11 @@ class ObjetIncidentTest {
   @Test
   void shouldRejectVehicleWithoutBrandOrModel() {
     ObjetIncident objet = ObjetIncident.builder()
-        .isVehicle(true)
-        .type(new RipolCode("200", "Velo"))
-        .build();
+      .isVehicle(true)
+      .type(new RipolCode("200", "Velo"))
+      .plaqueNumero("GE123456")
+      .plaquePays(SUISSE)
+      .build();
 
     assertThrows(ValidationMetierException.class, objet::champsObligatoire);
   }
@@ -97,11 +196,13 @@ class ObjetIncidentTest {
   @Test
   void shouldAcceptVehicleWithBrandAndModel() {
     ObjetIncident objet = ObjetIncident.builder()
-        .isVehicle(true)
-        .type(new RipolCode("200", "Velo"))
-        .fabricant(new RipolCode("TREK", "Trek"))
-        .modele(new RipolCode("DOMANE", "Domane"))
-        .build();
+      .isVehicle(true)
+      .type(new RipolCode("200", "Velo"))
+      .fabricant(new RipolCode("TREK", "Trek"))
+      .modele(new RipolCode("DOMANE", "Domane"))
+      .plaqueNumero("GE123456")
+      .plaquePays(SUISSE)
+      .build();
 
     assertDoesNotThrow(objet::champsObligatoire);
   }
@@ -109,12 +210,30 @@ class ObjetIncidentTest {
   @Test
   void shouldRejectVehicleWithOtherBrandOrModelWithoutPrecision() {
     ObjetIncident objet = ObjetIncident.builder()
-        .isVehicle(true)
-        .type(new RipolCode("200", "Velo"))
-        .fabricant(new RipolCode("AUTRE", "Autre"))
-        .modele(new RipolCode("AUTRE", "Autre"))
-        .build();
+      .isVehicle(true)
+      .type(new RipolCode("200", "Velo"))
+      .fabricant(new RipolCode("AUTRE", "Autre"))
+      .modele(new RipolCode("AUTRE", "Autre"))
+      .plaqueNumero("GE123456")
+      .plaquePays(SUISSE)
+      .build();
 
     assertThrows(ValidationMetierException.class, objet::champsObligatoire);
+  }
+
+  @Test
+  void shouldAcceptVehicleWithOtherBrandAndModelPrecision() {
+    ObjetIncident objet = ObjetIncident.builder()
+      .isVehicle(true)
+      .type(new RipolCode("200", "Velo"))
+      .fabricant(new RipolCode("AUTRE", "Autre"))
+      .fabricantAutre("Marque custom")
+      .modele(new RipolCode("AUTRE", "Autre"))
+      .modeleAutre("Modele custom")
+      .plaqueNumero("GE123456")
+      .plaquePays(SUISSE)
+      .build();
+
+    assertDoesNotThrow(objet::champsObligatoire);
   }
 }
--- pre-plainte-ihm/src/components/pre-plainte-component/event-info/VehiculeDetailsField.vue
+++ pre-plainte-ihm/src/components/pre-plainte-component/event-info/VehiculeDetailsField.vue
@@ -245,6 +245,7 @@
       v-model="plaqueNumero"
       :disabled="plaqueInconnu"
       class="mb-2"
+      :error-messages="plaqueNumeroError"
       variant="outlined"
       :hint="isPlaqueObligatoire ? t('incidentTypes.hintPlaqueNumeroObligatoire') : t('incidentTypes.hintPlaqueNumero')"
       persistent-hint
@@ -363,6 +364,7 @@ const {
   dateAchat,
   dateAchatError,
   plaqueNumero,
+  plaqueNumeroError,
   plaqueInconnu,
   plaquePays,
   plaqueCanton,
