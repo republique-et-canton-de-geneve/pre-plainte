cb802d640778592118919c63e80f7bc2a4131f6c
Corbel Killian (DIN) <killian.corbel@etat.ge.ch>
2026-06-03 13:20:12 +0200

    cypress : etape 3

--- pre-plainte-cypress/cypress/support/stubs/pre-plainte.js
+++ pre-plainte-cypress/cypress/support/stubs/pre-plainte.js
@@ -1,7 +1,7 @@
 export const stubSoumissionPrePlainteOk = (demandeId = "PPL-2026-0001") => {
   cy.intercept("POST", "**/api/preplainte/soumission", {
     statusCode: 200,
-    headers: { "content-type": "application/json" },
+    headers: { "content-type": "text/plain" },
     body: demandeId,
   }).as("submitPrePlainte");
 };
