885eda9d75ec1f974aaa23b324b19167e9326e9e
Corbel Killian (DIN) <killian.corbel@etat.ge.ch>
2026-06-04 11:49:46 +0200

    cypress : étape 4

--- pre-plainte-cypress/cypress/support/helpers/vuetify.js
+++ pre-plainte-cypress/cypress/support/helpers/vuetify.js
@@ -3,17 +3,13 @@ export const fieldRoot = (champ) => cy.contains("label", champ).parents(".v-inpu
 export const fieldInput = (champ) => fieldRoot(champ).find("input, textarea").first();
 
 export const fillField = (champ, valeur, options = {}) => {
-  const input = fieldInput(champ);
-
-  input.clear({ force: true });
-  input.type(valeur, { force: true, ...options });
+  fieldInput(champ).clear({ force: true });
+  fieldInput(champ).type(valeur, { force: true, ...options });
 };
 
 export const selectAutocomplete = (champ, valeur) => {
-  const input = fieldInput(champ);
-
-  input.click({ force: true });
-  input.clear({ force: true });
-  input.type(valeur, { force: true });
+  fieldInput(champ).click({ force: true });
+  fieldInput(champ).clear({ force: true });
+  fieldInput(champ).type(valeur, { force: true });
   cy.contains(".v-list-item-title", valeur).click({ force: true });
 };
