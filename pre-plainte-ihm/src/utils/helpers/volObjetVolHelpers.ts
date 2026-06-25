12958d68aecd5d07274a18df87fbd169a866da26
tissote <elise.tissot@etat.ge.ch>
2026-06-25 15:07:04 +0200

    fix: PPEL-460 : Revert
    # Conflicts:
    #	pre-plainte-core/src/main/java/ch/ge/police/core/domain/model/event/vol/common/ObjetIncident.java
    #	pre-plainte-ihm/src/components/pre-plainte-component/event-info/degat/DegatMaterielForm.vue
    #	pre-plainte-ihm/src/components/pre-plainte-component/event-info/degat/DegatVehiculeEndommageResumeSheet.vue
    #	pre-plainte-ihm/src/components/pre-plainte-component/event-info/vol/VolForm.vue
    #	pre-plainte-ihm/src/components/pre-plainte-component/event-info/vol/VolObjetVoleResumeSheet.vue
    #	pre-plainte-ihm/src/schemas/incident-evenement.schema.ts
    #	pre-plainte-ihm/src/views/Recapitulatif.vue
    #	pre-plainte-infrastructure/src/main/java/ch/ge/police/infrastructure/pdf/PdfGenerationAdapter.java

--- pre-plainte-ihm/src/utils/helpers/volObjetVolHelpers.ts
+++ pre-plainte-ihm/src/utils/helpers/volObjetVolHelpers.ts
@@ -71,7 +71,7 @@ export function libelleFabricantResumeVolObjet(obj: VolObjetFormSnapshot): strin
 }
 
 export function libelleModeleResumeVolObjet(obj: VolObjetFormSnapshot): string {
-  if (obj.modeleAutre) {
+  if (obj.modele?.code === "AUTRE" && obj.modeleAutre) {
     return obj.modeleAutre;
   }
   return obj.modele?.label ?? TEXTE_VIDE;
