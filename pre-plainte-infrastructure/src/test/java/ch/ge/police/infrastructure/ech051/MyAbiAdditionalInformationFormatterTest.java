ff1889c41a24f4d4bda47ce57a9da8c2457eebcb
Toure Maka (DIN) <maka.toure@etat.ge.ch>
2026-07-10 17:44:13 +0200

    correction import

--- pre-plainte-infrastructure/src/test/java/ch/ge/police/infrastructure/ech051/MyAbiAdditionalInformationFormatterTest.java
+++ pre-plainte-infrastructure/src/test/java/ch/ge/police/infrastructure/ech051/MyAbiAdditionalInformationFormatterTest.java
@@ -22,6 +22,29 @@ class MyAbiAdditionalInformationFormatterTest {
   }
 
   @Test
+  void format_shouldReturnNullForEmptyOrNullInput() {
+    assertNull(MyAbiAdditionalInformationFormatter.format(null));
+    assertNull(MyAbiAdditionalInformationFormatter.format(List.of()));
+    assertNull(MyAbiAdditionalInformationFormatter.format(List.of(" ", "")));
+  }
+
+  @Test
+  void booleanItem_shouldReturnOuiWhenTrue() {
+    assertEquals("Actif: oui", MyAbiAdditionalInformationFormatter.booleanItem("Actif", true));
+    assertNull(MyAbiAdditionalInformationFormatter.booleanItem("Actif", false));
+    assertNull(MyAbiAdditionalInformationFormatter.booleanItem("Actif", null));
+  }
+
+  @Test
+  void addLabeledAndAddBoolean_shouldIgnoreBlankValues() {
+    List<String> items = MyAbiAdditionalInformationFormatter.builder();
+    MyAbiAdditionalInformationFormatter.addLabeled(items, "Label", " ");
+    MyAbiAdditionalInformationFormatter.addBoolean(items, "Flag", false);
+
+    assertEquals(0, items.size());
+  }
+
+  @Test
   void labeledItem_shouldReturnNullWhenBlank() {
     assertNull(MyAbiAdditionalInformationFormatter.labeledItem("Label", " "));
   }
