19b4336ac608d583b421ed2fd3f1a52be4486dff
Toure Maka (DIN) <maka.toure@etat.ge.ch>
2026-07-20 11:58:12 +0200

    correction test en echec

--- pre-plainte-rest/src/test/java/ch/ge/police/ui/config/WebConfigTest.java
+++ pre-plainte-rest/src/test/java/ch/ge/police/ui/config/WebConfigTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.HandlerExecutionChain;
