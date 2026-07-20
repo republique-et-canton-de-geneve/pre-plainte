008a314e47aa053f8c9837ad0d0e30b5894f4068
Toure Maka (DIN) <maka.toure@etat.ge.ch>
2026-07-20 11:41:50 +0200

    correction fc + cybercrime en cyber-escroquerie

--- pre-plainte-rest/src/main/java/ch/ge/police/ui/config/WebConfig.java
+++ pre-plainte-rest/src/main/java/ch/ge/police/ui/config/WebConfig.java
@@ -1,5 +1,6 @@
 package ch.ge.police.ui.config;
 
+import ch.ge.police.infrastructure.security.FriendlyCaptchaInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -7,15 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  private final FriendlyCaptchaInterceptor friendlyCaptchaInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-      // No custom interceptor registration required for now.
-    }
+  public WebConfig(FriendlyCaptchaInterceptor friendlyCaptchaInterceptor) {
+    this.friendlyCaptchaInterceptor = friendlyCaptchaInterceptor;
+  }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{path:[^\\.]+}")
-                .setViewName("forward:/index.html");
-    }
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(friendlyCaptchaInterceptor)
+        .addPathPatterns(
+            "/api/preplainte/draft",
+            "/api/preplainte/soumission"
+        );
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/{path:[^\\.]+}")
+        .setViewName("forward:/index.html");
+  }
 }
